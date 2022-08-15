83ef08010179f4c184a0867c3edbdc8a90a19a28
Thiago Andriotti <T112171@perdigao.com.br>
2022-08-15 18:46:37 -0300

    Sumary component.

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,15 +1,35 @@
-import { ArrowCircleUp } from "phosphor-react"
-import { SummaryContainer } from "./styles"
+import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from "phosphor-react"
+import { SummaryCard, SummaryContainer } from "./styles"
 
 export function Summary() {
   return (
     <SummaryContainer>
-      <div>
+      <SummaryCard>
         <header>
           <span>inFlows</span>
-          <ArrowCircleUp />
+          <ArrowCircleUp size={32} color="#00b37e" />
         </header>
-      </div>
+
+        <strong>$ 18,200.00 </strong>
+      </SummaryCard>
+
+      <SummaryCard>
+        <header>
+          <span>outFlows</span>
+          <ArrowCircleDown size={32} color="#f75a68" />
+        </header>
+
+        <strong>$ 14,050.00 </strong>
+      </SummaryCard>
+
+      <SummaryCard variant="green">
+        <header>
+          <span>ammount</span>
+          <CurrencyDollar size={32} color="#fff" />
+        </header>
+
+        <strong>$ 4,150.00 </strong>
+      </SummaryCard>
     </SummaryContainer>
   )
 }
--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,8 +1,38 @@
 import { Header } from '../../components/Header'
+import { Summary } from '../../components/Summary'
+import { PriceHighlight, TransactionsContainer, TransactionsTable } from '../../components/Summary/styles'
 export function Transactions() {
   return (
     <div>
       <Header />
+      <Summary />
+
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Website development</td>
+              <td>
+                <PriceHighlight variant="income">
+                  $ 12,0000.00
+                </PriceHighlight>
+              </td>
+              <td>Services</td>
+              <td>05/08/2022</td>
+            </tr>
+            <tr>
+              <td width="50%">The Chefs</td>
+              <td>
+                <PriceHighlight variant="outcome">
+                  -R$ 59,00
+                </PriceHighlight>
+              </td>
+              <td>Food</td>
+              <td>10/08/2022</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </div>
 
   )
