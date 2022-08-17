1f272eff0bb8f56fc5471ec66d32a89c9fe2fc21
Thiago Andriotti <T112171@perdigao.com.br>
2022-08-16 21:35:27 -0300

    created a development server with json-server.

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,9 +1,32 @@
+import { useEffect, useState } from "react";
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
 import { SearchForm } from "./components/SearchForm";
 import { DateEnd, PriceHighlight, TransactionsContainer, TransactionsTable, ValueEnd } from "./styles";
 
+interface Transaction {
+  id: number;
+  description: string;
+  type: 'income' | 'outcome';
+  price: number;
+  category: string;
+  createdAt: string;
+}
+
 export function Transactions() {
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+
+  async function loadTransactions() {
+    const response = await fetch('http://localhost:3337/transactions')
+    const data = await response.json()
+
+    setTransactions(data)
+  }
+
+  useEffect(() => {
+    loadTransactions()
+  }, []);
+
   return (
     <div>
       <Header />
@@ -13,28 +36,24 @@ export function Transactions() {
 
         <SearchForm />
         <TransactionsTable>
-          <tbody>
-            <tr>
-              <td width="50%">Website development</td>
-              <ValueEnd>
-                <PriceHighlight variant="income">
-                  $ 12,000.00
-                </PriceHighlight>
-              </ValueEnd>
-              <td>Services</td>
-              <DateEnd>05/08/2022</DateEnd>
-            </tr>
-            <tr>
-              <td width="50%">The Chefs</td>
-              <ValueEnd>
-                <PriceHighlight variant="outcome">
-                  -$ 59.00
-                </PriceHighlight>
-              </ValueEnd>
-              <td>Food</td>
-              <DateEnd>09/08/2022</DateEnd>
-            </tr>
-          </tbody>
+          {transactions.map(transaction => {
+            return (
+              <tbody>
+                <tr>
+                  <td width="50%">{transaction.description}</td>
+                  <ValueEnd>
+                    <PriceHighlight variant={transaction.type}>
+                      {transaction.price}
+                    </PriceHighlight>
+                  </ValueEnd>
+                  <td>{transaction.category}</td>
+                  <DateEnd>{transaction.createdAt}</DateEnd>
+                </tr>
+
+              </tbody>
+            )
+          })}
+
         </TransactionsTable>
       </TransactionsContainer>
     </div>
